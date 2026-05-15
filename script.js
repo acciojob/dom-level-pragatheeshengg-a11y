c2b78324aa4e031a91573641e93fc5c25b28690d
pragatheeshengg-a11y <pragatheeshengg@gmail.com>
2026-05-15 12:23:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //your JS code here. If required.
-let ele=document.GetElementById('level');
+let ele=document.getElementById('level');
 let lvl=0;
 while(ele){
 	lvl++;
 	ele=ele.parentElement;
 }
-alert(`The level of the element is:${lvl}``);
+alert(`The level of the element is:${lvl}`);
